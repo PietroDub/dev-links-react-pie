117fd010877df829ce91670ddea20a8f73e0ef6d
PietroDub <pietroti453@gmail.com>
2023-08-11 11:05:42 -0300

    switch button OK

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,24 +1,26 @@
 import Profile from "../components/profiles/profile";
-import"./App.css"
+import "./App.css";
+import Switch from "../components/switch/switch";
 
 import imgDarkMode from "../img/SUSS.webp";
 import imglightMode from "../img/sus.webp";
 import { useState } from "react";
 
 const App = () => {
- const [islightMode,setIsLightMode ] = useState (false);
+  const [islightMode, setIsLightMode] = useState(false);
 
-
- const imgScr=islightMode ? imglightMode : imgDarkMode;
-const imgAlt = islightMode ? "imagem no  modo claro" : "Imagem no Modo Escuro"
-return(
-<div>
-
-  <Profile
-    imgSrc={imgScr} imgAlt={imgAlt}
-  >@Vamosdub</Profile>
-  </div>
-)
-}
+  const imgScr = islightMode ? imglightMode : imgDarkMode;
+  const imgAlt = islightMode
+    ? "imagem no  modo claro"
+    : "Imagem no Modo Escuro";
+  return (
+    <div id="container">
+      <Profile imgSrc={imgScr} imgAlt={imgAlt}>
+        @Vamosdub
+      </Profile>
+      <Switch></Switch>
+    </div>
+  );
+};
 
 export default App;
