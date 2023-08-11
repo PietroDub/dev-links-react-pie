fddaaf009926f628180a80f76db96a971cebe0cd
PietroDub <pietroti453@gmail.com>
2023-08-11 13:59:51 -0300

    Terminado links , começado social links

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,6 +1,7 @@
 import Profile from "../components/profiles/profile";
 import "./App.css";
 import Switch from "../components/switch/switch";
+import Links from "../components/links/links";
 
 import imgDarkMode from "../img/SUSS.webp";
 import imglightMode from "../img/sus.webp";
@@ -19,6 +20,14 @@ const App = () => {
         @Vamosdub
       </Profile>
       <Switch></Switch>
+      <div id="links">
+        <ul>
+          <Links link={"https://github.com/PietroDub"}>Git hub</Links>
+          <Links link={"https://www.youtube.com/channel/UCRapgt19yeSs82V_Sb-AljA"}>Youtube</Links>
+          <Links link={""}>Instagram</Links>
+          <Links link={""}>Projetos</Links>
+        </ul>
+      </div>
     </div>
   );
 };
