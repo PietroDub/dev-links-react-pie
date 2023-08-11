924ddc9fc196bace5b3d41b0ff79b9d5ca6f0b71
PietroDub <pietroti453@gmail.com>
2023-08-11 17:11:19 -0300

    semi finalizado 1

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -10,34 +10,41 @@ import imglightMode from "../img/sus.webp";
 import { useState } from "react";
 
 const App = () => {
-  const [islightMode, setIsLightMode] = useState(false);
-
+  const [islightMode, setIsLightMode] = useState(true);
+  const toggleMode = () => {
+    setIsLightMode((prevMode) => !prevMode);
+  };
   const imgScr = islightMode ? imglightMode : imgDarkMode;
   const imgAlt = islightMode
     ? "imagem no  modo claro"
     : "Imagem no Modo Escuro";
   return (
-    <div id="container">
-      <Profile imgSrc={imgScr} imgAlt={imgAlt}>
-        @Vamosdub
-      </Profile>
-      <Switch></Switch>
-      <div id="links">
-        <ul>
-          <Links link={"https://github.com/PietroDub"}>Git hub</Links>
-          <Links link={"https://www.youtube.com/channel/UCRapgt19yeSs82V_Sb-AljA"}>Youtube</Links>
-          <Links link={""}>Instagram</Links>
-          <Links link={""}>Projetos</Links>
-        </ul>
+    <div id="App" className={islightMode ? "light" : ""}>
+      <div id="container">
+        <Profile imgSrc={imgScr} imgAlt={imgAlt}>
+          @Vamosdub
+        </Profile>
+        <Switch mode={toggleMode} infoButton={islightMode} />
+        <div id="links">
+          <ul>
+            <Links link={"https://github.com/PietroDub"}>Git hub</Links>
+            <Links
+              link={"https://www.youtube.com/channel/UCRapgt19yeSs82V_Sb-AljA"}
+            >
+              Youtube
+            </Links>
+            <Links link={""}>Instagram</Links>
+            <Links link={""}>Projetos</Links>
+          </ul>
+        </div>
+        <div id="socialLinks">
+          <Social href={"#"} icon={"logo-github"} />
+          <Social href={"#"} icon={"logo-youtube"} />
+          <Social href={"#"} icon={"logo-instagram"} />
+          <Social href={"#"} icon={"logo-tiktok"} />
+        </div>
+        <Footer link={"#"}>@Vamosdub</Footer>
       </div>
-      <div id="socialLinks">
-        <Social href={"#"} icon={"logo-github"}/>
-        <Social href={"#"} icon={"logo-youtube"}/>
-        <Social href={"#"} icon={"logo-instagram"}/>
-        <Social href={"#"} icon={"logo-tiktok"}/>
-
-      </div>
-      <Footer link={"#"}>@Vamosdub</Footer>
     </div>
   );
 };
